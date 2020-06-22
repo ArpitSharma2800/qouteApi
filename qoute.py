e92407b61df407abdc2ca3b8d54f5f9b55a57739
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2020-06-22 10:03:31 +0530

    first commit

---
+++ qoute.py
@@ -0,0 +1,63 @@
+from flask import Flask
+from flask_json_schema import JsonSchema, JsonValidationError
+from flask_pymongo import PyMongo
+from pymongo import MongoClient, UpdateOne
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from flask import jsonify, request
+import os
+from flask_cors import CORS, cross_origin
+
+
+app = Flask(__name__)
+cors = CORS(app)
+schema = JsonSchema(app)
+client = MongoClient(
+    "mongodb+srv://arpit:iluvmuma@arpit-xcm5n.gcp.mongodb.net/test?retryWrites=true&w=majority")
+
+qoute_schema = {
+    'uuid': {'type': "string"},
+    'properties': {
+        "qoute": {
+            "type": "array",
+            "items":  {
+                "type": "object",
+                "properties": {
+                    "title": {'type': "string"}
+                }
+            },
+        }
+    }
+}
+
+
+@app.route('/add', methods=['POST'])
+@cross_origin()
+@schema.validate(qoute_schema)
+def add_user():
+    users = client.users.qoute.find_one({'uuid': request.json['uuid']})
+    if(users):
+        id = request.json['uuid']
+        data = request.json['qoute']
+        print(data)
+        client.users.qoute.update_one(
+            {'uuid': id}, {'$addToSet':  {"qoute": data[0]}})
+        response = jsonify("User updated successfully")
+        response.status_code = 200
+        return response
+    else:
+        print(request.json)
+        client.users.qoute.insert(request.json)
+        return jsonify({'success': True, 'message': 'qouteList '})
+
+
+@app.route('/users')
+@cross_origin()
+def users():
+    users = client.users.qoute.find()
+    resp = dumps(users)
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
